9b8a1f9baaade2f5f50e9cf1be9faf15674e2ca6
Anisha <anisharuhisingh7@gmail.com>
2025-11-27 14:46:11 +0530

    Updated the message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,5 +4,6 @@ public class App {
 
 	public static void main(String[] args) {
         System.out.println("Hello World from Maven JAR created in Eclipse!");
+        System.out.println("Hello World From GitHub now");
     }
 }
