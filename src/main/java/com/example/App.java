bbe0721a4e47449d1e0ce84db92d50dfbbae94bf
Anisha <anisharuhisingh7@gmail.com>
2025-12-07 16:17:14 +0530

    trying for autotrigger

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,7 +5,7 @@ public class App {
 	public static void main(String[] args) {
         System.out.println("Hello World from Maven JAR created in Eclipse!");
         System.out.println("Hello World From GitHub now");
-        System.out.println("Writing 3rd line to check autometic build trigger.");
+        System.out.println("Writing 3rd line to check autometic build trigger");
         System.out.println("Using GitHub Actions..");
         System.out.println("Let's try..");
     }
