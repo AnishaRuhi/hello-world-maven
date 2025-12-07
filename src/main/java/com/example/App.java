d58fcf0519c88f3254575769a0a6125c54a88888
Anisha <anisharuhisingh7@gmail.com>
2025-12-07 16:12:17 +0530

    test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,8 +5,7 @@ public class App {
 	public static void main(String[] args) {
         System.out.println("Hello World from Maven JAR created in Eclipse!");
         System.out.println("Hello World From GitHub now");
-        System.out.println("Writing 3rd line to check autometic build trigger");
-        System.out.println("Writing 4th line to check autometic build trigger using PAT... New...Welcome");
+        System.out.println("Writing 3rd line to check autometic build trigger.");
         System.out.println("Using GitHub Actions..");
         System.out.println("Let's try..");
     }
